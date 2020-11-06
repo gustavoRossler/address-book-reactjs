5f926b478ab8c593301aefa07577b5048aa2ae50
Gustavo Pereira Rossler <gustavo_rossler@hotmail.com>
2020-11-06 10:51:13 -0300

    Local storage functionality created

--- src/contexts/AddressesContext.js
+++ src/contexts/AddressesContext.js
@@ -1,11 +1,16 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import { addressesReducer } from '../reducers/addressesReducer';
 
 export const AddressesContext = createContext();
 
 const AddressesContextProvider = (props) => {
-    const [addresses, dispatch] = useReducer(addressesReducer, []);
-
+    const [addresses, dispatch] = useReducer(addressesReducer, [], () => {
+        const localData = localStorage.getItem('addresses');
+        return localData ? JSON.parse(localData) : [];
+    });
+    useEffect(() => {
+        localStorage.setItem('addresses', JSON.stringify(addresses));
+    }, [addresses]);
     return (
         <AddressesContext.Provider value={{ addresses, dispatch }}>
             {props.children}
