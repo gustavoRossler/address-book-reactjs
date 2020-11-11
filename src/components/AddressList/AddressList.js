72cc3b33f90de9c1a33bc12ec94b9fd53b1ab4de
Gustavo Pereira Rossler <gustavo_rossler@hotmail.com>
2020-11-11 12:19:37 -0300

    Fixed the sort method, and separated the addresses list in two arrays

--- src/components/AddressList/AddressList.js
+++ src/components/AddressList/AddressList.js
@@ -14,15 +14,18 @@ const AddressList = () => {
     const [sortByName, setSortByName] = useState('');
     const [sortByAddress, setSortByAddress] = useState('');
 
-    const handleSortByAddress = () => {
+    const [fixedAddresses, setFixedAddresses] = useState([]);
+    const [normalAddresses, setNormalAddresses] = useState([]);
+
+    const handleSortByAddress = (dir) => {
         setSortByName('');
-        setSortByAddress((sortByAddress === 'asc') ? 'desc' : 'asc');
+        setSortByAddress(dir);
 
     }
 
-    const handleSortByName = () => {
+    const handleSortByName = (dir) => {
         setSortByAddress('');
-        setSortByName((sortByName === 'asc') ? 'desc' : 'asc');
+        setSortByName(dir);
 
     }
 
@@ -69,28 +72,33 @@ const AddressList = () => {
     }
 
     const sortList = (a, b) => {
-        if (b.defaultShippingAddress || b.billingAddress) {
-            return 1;
-        }
+        
         if (sortByName !== '') {
             if (sortByName === 'asc') {
-                return (a.name.replace(/ /g, '') < b.name.replace(/ /g, '')) ? 1 : -1;
-            } else {
                 return (a.name.replace(/ /g, '') > b.name.replace(/ /g, '')) ? 1 : -1;
+            } else {
+                return (a.name.replace(/ /g, '') < b.name.replace(/ /g, '')) ? 1 : -1;
             }
         } else if (sortByAddress !== '') {
             if (sortByAddress === 'asc') {
-                return (a.address.replace(/ /g, '') < b.address.replace(/ /g, '')) ? 1 : -1;
-            } else {
                 return (a.address.replace(/ /g, '') > b.address.replace(/ /g, '')) ? 1 : -1;
+            } else {
+                return (a.address.replace(/ /g, '') < b.address.replace(/ /g, '')) ? 1 : -1;
             }
         }
+
         return 0;
     }
 
     useEffect(() => {
+        if (sortByName === '' && sortByAddress === '') {
+            setSortByName('asc');
+        }
+
         addresses.sort((a, b) => sortList(a, b));
-    }, [sortByAddress, sortByName, addresses]);
+        setFixedAddresses(addresses.filter(item => item.defaultShippingAddress || item.billingAddress));
+        setNormalAddresses(addresses.filter(item => !item.defaultShippingAddress && !item.billingAddress));
+    }, [sortByName, sortByAddress, addresses.length, showModalForm]);
 
     return (
         <>
@@ -105,12 +113,12 @@ const AddressList = () => {
             <Table bordered hover>
                 <thead>
                     <tr>
-                        <th onClick={handleSortByName} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
+                        <th onClick={() => handleSortByName((sortByName === 'asc') ? 'desc' : 'asc')} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
                             {(sortByName === 'desc') ? <FaSortAlphaDownAlt /> : ''}
                             {(sortByName === 'asc') ? <FaSortAlphaDown /> : ''}
                             &nbsp;Nome
                         </th>
-                        <th onClick={handleSortByAddress} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
+                        <th onClick={() => handleSortByAddress((sortByAddress === 'asc') ? 'desc' : 'asc')} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
                             {(sortByAddress === 'desc') ? <FaSortAlphaDownAlt /> : ''}
                             {(sortByAddress === 'asc') ? <FaSortAlphaDown /> : ''}
                             Endereço
@@ -120,22 +128,42 @@ const AddressList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {(addresses.length > 0) ? addresses.map(address =>
-                        <tr key={address.id} style={{ background: (address.defaultShippingAddress || address.billingAddress) ? '#fff9de' : '#fff' }}>
-                            <td>{address.name}</td>
-                            <td>{address.address}</td>
-                            <td>{address.city}/{address.uf}</td>
-                            <td>
-                                <Button type="button" variant="secondary" size="sm"
-                                    onClick={() => viewAddress(address)}><FaEye /> Visualisar</Button>
+                    {(normalAddresses.length > 0 || fixedAddresses.length > 0) ? (
+                        fixedAddresses.map(address =>
+                            <tr key={address.id} style={{ background: (address.defaultShippingAddress || address.billingAddress) ? '#fff9de' : '#fff' }}>
+                                <td>{address.name}</td>
+                                <td>{address.address}</td>
+                                <td>{address.city}/{address.uf}</td>
+                                <td>
+                                    <Button type="button" variant="secondary" size="sm"
+                                        onClick={() => viewAddress(address)}><FaEye /> Visualisar</Button>
                                 &nbsp;&nbsp;&nbsp;
                                 <Button type="button" variant="primary" size="sm"
-                                    onClick={() => editAddress(address)}><FaEdit /> Editar</Button>
+                                        onClick={() => editAddress(address)}><FaEdit /> Editar</Button>
                                 &nbsp;&nbsp;&nbsp;
                                 <Button type="button" variant="danger" size="sm"
-                                    onClick={() => confirmRemoveAddress(address.id)}><FaTrash /> Excluir</Button>
-                            </td>
-                        </tr>
+                                        onClick={() => confirmRemoveAddress(address.id)}><FaTrash /> Excluir</Button>
+                                </td>
+                            </tr>
+                        )
+                    ).concat(
+                        normalAddresses.map(address =>
+                            <tr key={address.id} style={{ background: (address.defaultShippingAddress || address.billingAddress) ? '#fff9de' : '#fff' }}>
+                                <td>{address.name}</td>
+                                <td>{address.address}</td>
+                                <td>{address.city}/{address.uf}</td>
+                                <td>
+                                    <Button type="button" variant="secondary" size="sm"
+                                        onClick={() => viewAddress(address)}><FaEye /> Visualisar</Button>
+                                &nbsp;&nbsp;&nbsp;
+                                <Button type="button" variant="primary" size="sm"
+                                        onClick={() => editAddress(address)}><FaEdit /> Editar</Button>
+                                &nbsp;&nbsp;&nbsp;
+                                <Button type="button" variant="danger" size="sm"
+                                        onClick={() => confirmRemoveAddress(address.id)}><FaTrash /> Excluir</Button>
+                                </td>
+                            </tr>
+                        )
                     ) : (<tr>
                         <td colSpan="4" className="text-center">Nenhum endereço cadastrado, clique no botão adicionar para começar.</td>
                     </tr>)}
